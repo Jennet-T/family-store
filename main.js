19f5f53f8375a5cdf2c82cc7c82830d540a21dd2
Hudayar-T <tash.hudayar@gmail.com>
2023-03-24 22:31:20 +0300

    backend added

--- main.js
+++ main.js
@@ -1,46 +1,106 @@
-// BUTTON EQUAL DISTANCE
-// setTimeout(function(){
-//     const add = document.getElementById('add_button')
-//     const tasks = document.getElementById('tasks_button')
-//     const history = document.getElementById('history_button')
-//     const header = document.getElementById('header').offsetWidth
-
-//     console.log("Header: " + header)
-//     console.log("Add: " + add.offsetWidth)
-//     console.log("Tasks: " + tasks.offsetWidth)
-//     console.log("History: " + history.offsetWidth)
+setTimeout(function(){
+    //DOBAW ortasyna koymagyn kody
+    const btn = document.getElementById('dobaw_button')
+    const add_table = document.getElementById('add_table')
+    const add = document.getElementById('add')
     
-//     add.style.marginLeft = (header - add.offsetWidth - tasks.offsetWidth - history.offsetWidth) / 4.3
-//     tasks.style.marginLeft = (header - add.offsetWidth - tasks.offsetWidth - history.offsetWidth) / 4.3
-//     history.style.marginLeft = (header - add.offsetWidth - tasks.offsetWidth - history.offsetWidth) / 4.3
+    btn.style.marginTop = (add_table.offsetHeight/2) + 'px'
+    
+    //ADD_TABLE-y iza sushurmali
+    add.style.left = '-' + (add_table.offsetWidth + 1) + 'px'
+    add.style.top = (document.body.offsetHeight / 2 - add.offsetHeight/2) + 'px'
 
-// },10)
+    //DATABASE-dan produktalary get
 
+    const shablon = document.getElementById('shablon')
+    $.post('work.php',
+    {
+        'get_products': true
+    },
+    function(str){
+        data = JSON.parse(str)
+        
+        RenameShablon(shablon, data)
+    })
 
+},10)
 
 function ShowHideProducts(elem)
 {
     const store = elem.parentNode;
     const products = store.children[1];
-    if(products.style.display == "none")
+    if(products.style.height == "0px")
     {
-        products.style.display = "block"
+        curHeight = $(products).height()
+        autoHeight = $(products).css('height', 'auto').height();
+        $(products).height(curHeight).animate({height: autoHeight}, 600);
     }
     else
     {
-        products.style.display = "none"
+        $(products).animate({height: "0px"}, 600)
     }
 }
 
-function CheckProduct(elem){
+function CheckProduct(elem)
+{
     elem.children[0].checked = !elem.children[0].checked
-    
-    if(elem.children[1].style.textDecoration == "line-through")
-    {
-        elem.children[1].style.textDecoration = "none"
+    elem.children[1].classList.toggle("clicked")
+}
+
+function DragAdd(elem)
+{
+    const add = elem.parentNode;
+
+    if(add.style.left == '0px')
+    {  
+        $(add).animate({left: '-' + (document.getElementById('add_table').offsetWidth + 1) + 'px'}, 600)
     }
     else
     {
-        elem.children[1].style.textDecoration = "line-through"
+        $(add).animate({left: '0px'}, 600)
     }
+}
+
+function Dobaw()
+{
+    const product = document.getElementById('produkta_input')
+    const shop = document.getElementById('magazin_select')
+    const amount = document.getElementById('amount_input')
+    const type = document.getElementById('amount_ta_kg')
+
+    product.style.borderColor = "black"
+    amount.style.borderColor = "black"
+
+    if(product.value == '') product.style.borderColor = "red"
+    else if(amount.value == '') amount.style.borderColor = "red"
+    else{
+        $.post("work.php",
+        {
+            'product': product.value,
+            'shop': shop.value,
+            'amount': amount.value,
+            'type': type.value,
+            'dobaw_products': 'dobaw'
+        },
+        function(data){
+            console.log(data)
+        })
+    }
+}
+
+function RenameShablon(shbl, data)
+{
+    data.forEach(store => {
+        const tshbl = shbl.cloneNode(true)
+        const sproduct = tshbl.children[1].children[0].cloneNode(true);
+        tshbl.children[1].children[0].remove()
+        tshbl.id = store[0].magazin.replace(/ /g, "_")
+        tshbl.children[0].children[0].innerText = store[0].magazin
+        store.forEach(product => {
+            const tsproduct = sproduct.cloneNode(true);
+            tsproduct.children[1].innerText = product.name
+            tshbl.children[1].appendChild(tsproduct)
+        });
+        document.getElementById('table').appendChild(tshbl)
+    });
 }
---
+++ work.php
@@ -0,0 +1,60 @@
+<?php
+
+    $con = new mysqli('localhost', 'root', '', 'family_store');
+
+    if($con->connect_error)
+    {
+        die("Connection failed: " . $con->connect_error);
+    }
+
+    if(isset($_POST['dobaw_products']))
+    {
+        $product = $_POST['product'];
+        $shop = $_POST['shop'];
+        $type = $_POST['type'];
+        $amount = $_POST['amount'];
+        
+        $sql = "insert into products 
+        (name, added_date, magazin, amount) 
+        values ('". $product . "', 
+        '" . date("Y-m-d h:i:sa") . "', 
+        '" . $shop . "', 
+        '" . $amount . $type . "')";
+
+        if($con->query($sql) === true)
+        {
+            echo 'successs!!!!';
+        }
+        else
+        {
+            echo 'Eroor: ' . $con->error;
+        }
+
+    }
+
+    if(isset($_POST['get_products']))
+    {
+        $products = array();
+        $sql1 = 'select magazin from products group by magazin';
+        $query1 = $con->query($sql1);
+
+        if($query1->num_rows > 0)
+        {
+            while($row1 = $query1->fetch_assoc())
+            {
+                $sql2 = 'select name, magazin, amount from products where magazin="'. $row1["magazin"] .'";';
+                $query2 = $con->query($sql2);
+                $store = array();
+                while($row2 = $query2->fetch_assoc())
+                {
+                    array_push($store, $row2);
+                }
+                array_push($products, $store);
+            }
+        }
+        echo json_encode($products);
+    }
+    
+    $con->close();
+
+?>
